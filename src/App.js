e1c63a183e295c8e0d1df57e1d7f5604ba6c2dad
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2022-03-31 12:25:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import Banner from './Components/Banner.js';
-import Footer from './Components/Footer.js';
-import './App.css';
+import React from "react";
+import Banner from "./Components/Banner.js";
+import Footer from "./Components/Footer.js";
+import Population from "./Components/Population.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <h1>NYC Demographics Dashboard</h1>
-      
+
       <div className="visCard">
-        <p>Add a visualization here</p>
+        <Population />
       </div>
-      
+
       <Footer />
     </div>
   );
